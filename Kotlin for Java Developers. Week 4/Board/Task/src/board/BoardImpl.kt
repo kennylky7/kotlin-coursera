ca636b807498f54c5eba2e514604fbe21f3c6cdc
Kenny Lai <kennylky7@gmail.com>
2023-11-07 12:05:47 +0000

    First commit finished BoardImpl.kt

---
+++ Kotlin for Java Developers. Week 4/Board/Task/src/board/BoardImpl.kt
@@ -0,0 +1,79 @@
+package board
+
+import board.Direction.*
+import board.SquareBoard.SquareBoard.getCellKey
+
+class SquareBoardImpl(inWidth: Int) : SquareBoard {
+
+    private val cellMap = hashMapOf<String, Cell>()
+    override val width: Int = inWidth
+
+    init {
+        for (row in 1..width) {
+            for (col in 1..width) {
+                val keyStr = getCellKey(row, col)
+                cellMap[keyStr] = Cell(row, col)
+            }
+        }
+    }
+
+    override fun getCellOrNull(i: Int, j: Int): Cell? {
+        return cellMap[getCellKey(i, j)]
+    }
+
+    override fun getCell(i: Int, j: Int): Cell {
+        return getCellOrNull(i, j) ?: throw IllegalArgumentException("Cell[$i, $j] not exist")
+    }
+
+    override fun getAllCells(): Collection<Cell> {
+        val resultList = mutableListOf<Cell>()
+        for (row in 1..width) {
+            for (col in 1..width) {
+                val keyStr = getCellKey(row, col)
+                cellMap[keyStr]?.let { resultList.add(it) }
+            }
+        }
+        return resultList.toList()
+    }
+
+    override fun getRow(i: Int, jRange: IntProgression): List<Cell> {
+        val resultList = mutableListOf<Cell>()
+        for (col in jRange) {
+            val keyStr = getCellKey(i, col)
+            cellMap[keyStr].let {
+                if (it != null) resultList += it
+            }
+        }
+        return resultList
+    }
+
+    override fun getColumn(iRange: IntProgression, j: Int): List<Cell> {
+        val resultList = mutableListOf<Cell>()
+        for (row in iRange) {
+            val keyStr = getCellKey(row, j)
+            cellMap[keyStr].let {
+                if (it != null) resultList += it
+            }
+        }
+        return resultList
+    }
+
+    override fun Cell.getNeighbour(direction: Direction): Cell? {
+        val (row, col) = when (direction) {
+            UP -> this.i - 1 to this.j
+            DOWN -> this.i + 1 to this.j
+            LEFT -> this.i to this.j - 1
+            RIGHT -> this.i to this.j + 1
+        }
+
+        return if (row < 1 || row > width) null
+        else if (col < 1 || col > width) null
+        else cellMap[getCellKey(row, col)]
+    }
+
+}
+
+
+fun createSquareBoard(width: Int): SquareBoard = SquareBoardImpl(width)
+fun <T> createGameBoard(width: Int): GameBoard<T> = TODO()
+
